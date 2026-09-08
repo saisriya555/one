0b7d8ce814e64c776aed34c22527618465c6d268
saisriya555 <95363537+saisriya555@users.noreply.github.com>
2026-09-08 15:23:42 +0530

    Fix alert message in contact form submission
    Updated alert message to clarify response timing.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -6,7 +6,7 @@ document.getElementById('contact-form').addEventListener('submit', function (e) 
   const message = document.getElementById('message').value;
 
   if (name && email && message) {
-    alert('Thank you for contacting us! We will get back to you as as soon as possible.we will let you know');
+    alert('Thank you for contacting us! We will get back to you as as soon as possible.we will let you know when we are back');
     document.getElementById('contact-form').reset();
   } else {
     alert('Please fill out all fields.');
